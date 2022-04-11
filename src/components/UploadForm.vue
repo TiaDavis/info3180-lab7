72f74e0d6e752ab4d4072c60d8dbe794d519357e
TiaDavis <tiajhaedavis258@gmail.com>
2022-04-10 20:20:26 -0500

    Final updates

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,13 +1,14 @@
 <template>
-     <span id="msg" class=" form-control hide alert" ></span>
-    <form @submit.prevent="uploadPhoto" method="POST" id="uploadForm" enctype = "multipart/form-data">
+     <span id="msg" class=" form-control alert" >
+     </span>
+    <form @submit.prevent="uploadPhoto" method="POST" id="uploadForm" >
         <div class="form-group">
-            <label for="description"> Description</label>
+            <label for="description">Description</label>
             <textarea class="form-control" id="description" rows="3" name="description" ></textarea>
         </div>
 
         <div class="form-group">
-            <label for="photo">Photo Upload</label>
+            <label for="photo">Photo Upload</label><br>
             <input type="file" class="form-control-file" id="photo"  name="photo" >
         </div>
 
@@ -41,6 +42,7 @@
                     return response.json();
                 })
                 .then(function (data) {
+                    // display a success message
                     alertcontainer.classList.remove('hide');
                     if (Array.isArray(data)){
                         alertcontainer.innerHTML = '';
@@ -48,19 +50,37 @@
                         alertcontainer.classList.remove('alert-success');
                         alertcontainer.classList.add('alert-danger');
                         let errorlist = data;
+                        let fields = [];
                         for (let err=0; err<errorlist.length; err++){
                             const erroritem = document.createElement("li");
                             const node = document.createTextNode(errorlist[err]);
                             erroritem.appendChild(node);
                             alertcontainer.appendChild(erroritem);
+                            fields.push(errorlist[err].split(" ")[3]);
+                        }
+                        console.log(fields);
+                        if (fields.includes("description") && !fields.includes("photo")){
+                            document.querySelector('#description').classList.add('errhighlight');
+                            document.querySelector('#photo').classList.remove('errhighlight');
+                        }
+                        if(fields.includes("photo") && !fields.includes("description")){
+                            document.querySelector('#photo').classList.add('errhighlight');
+                            document.querySelector('#description').classList.remove('errhighlight');
+                        }
+                        if (fields.includes("description") && fields.includes("photo")){
+                            document.querySelector('#description').classList.add('errhighlight');
+                            document.querySelector('#photo').classList.add('errhighlight');
                         }
                        
                     }
                     else{
+                        document.querySelector('#description').classList.remove('errhighlight');
+                        document.querySelector('#photo').classList.remove('errhighlight');
                         alertcontainer.classList.add('alert-success');
                         alertcontainer.classList.remove('alert-danger');
                         alertcontainer.innerHTML= data.message;
                     }
+                    console.log(data);
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -86,4 +106,10 @@
 </script>
 
 <style>
+form *{
+    margin-bottom: 1em;
+}
+.errhighlight{
+    box-shadow: #F8D7DA 0px 3px 6px, #F8D7DA 0px 3px 6px;
+}
 </style>
